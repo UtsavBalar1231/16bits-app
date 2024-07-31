62a2befcc0ecaaa72b32b6645f1773bb49eb696d
UtsavBalar1231 <utsavbalar1231@gmail.com>
2024-07-31 10:58:12 +0530

    16bits: Fix paddings and margin in navigation menu
    Change-Id: I87c263e64b8fcee24f003b73eb5fe81d9f02610d
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -18,17 +18,17 @@ function Navigation() {
         <motion.div variants={menuSlide} initial="initial" animate="enter" exit="exit" className="fixed right-0 top-0 h-screen bg-black-absolute text-white dark:bg-white-absolute dark:text-black">
             <div className="box-border h-full p-[6rem] flex flex-col justify-between">
                 <div onMouseLeave={() => { setSelectedIndicator(window.location.pathname) }} className="flex flex-col text-6xl gap-2 mt-[5rem]">
-                    <div className="border-b border-white dark:border-black uppercase text-sm mb-[2.5rem] font-minecraft">
+                    <div className="border-b border-white dark:border-black uppercase text-sm mb-[1rem] font-minecraft p-[0.5rem]">
                         <p>Navigation</p>
                     </div>
                     {navigation.map((item: NavigationItem, _index: number) => (
                         <motion.div className="relative flex items-center" onMouseEnter={() => { setSelectedIndicator(item.url) }} custom={item.id} variants={slide} initial="initial" animate="enter" exit="exit">
-                            <motion.div variants={scale} animate={selectedIndicator == item.url ? "open" : "closed"} className="w-[0.5rem] h-[0.5rem] bg-code-09 dark:bg-code-18 rounded-full absolute right-[30px]">
+                            <motion.div variants={scale} animate={selectedIndicator == item.url ? "open" : "closed"} className="w-[0.5rem] h-[0.5rem] bg-code-09 dark:bg-code-18 rounded-full absolute right-[1rem]">
                             </motion.div>
                             <Link
                                 key={item.id}
                                 to={item.url}
-                                className='no-underline text-white hover:text-code-09 dark:text-black-absolute dark:hover:text-code-18 transition-colors text-3xl font-minecraft'
+                                className='no-underline text-white hover:text-code-09 dark:text-black-absolute dark:hover:text-code-18 transition-colors text-3xl font-minecraft p-[0.5rem]'
                             >
                                 {item.title}
                             </Link>
